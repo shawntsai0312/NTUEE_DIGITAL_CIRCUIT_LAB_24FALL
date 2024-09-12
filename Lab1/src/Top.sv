b142e0b34f6916e5fa0c898bbe9380627bcf0efc
shawntsai0312 <shawntsai0312@gmail.com>
2024-09-12 10:30:27 +0800

    basic lfsr done

--- Lab1/src/Top.sv
+++ Lab1/src/Top.sv
@@ -17,6 +17,8 @@ logic [3:0] o_random_out_r, o_random_out_w;
 // ===== Registers & Wires =====
 logic state_r, state_w;
 
+logic [15:0] lfsr_r, lfsr_w; // taps = 15, 14, 12, 3
+
 // ===== Output Assignments =====
 assign o_random_out = o_random_out_r;
 
@@ -24,7 +26,7 @@ assign o_random_out = o_random_out_r;
 
 // state transition
 always_comb begin
-	state_w        = state_r;
+	state_w = state_r;
 
 	case(state_r)
 	S_IDLE: begin
@@ -32,7 +34,6 @@ always_comb begin
 			state_w = S_PROC;
 		end
 	end
-
 	S_PROC: begin
 		if (i_start) begin
 			state_w = (o_random_out_r == 4'd10) ? S_IDLE : state_w;
@@ -41,19 +42,46 @@ always_comb begin
 	endcase
 end
 
-// random number generator
+// lfsr transition
+// taps = 15, 14, 12, 3
 always_comb begin
-	o_random_out_w = (state_r == S_PROC) ? $urandom_range(0, 16) : 4'd0;
+	lfsr_w = lfsr_r;
+
+	case(state_r)
+	S_IDLE: begin
+		lfsr_w = lfsr_r;
+	end
+	S_PROC: begin
+		lfsr_w[15:1] = lfsr_r[14:0];
+		lfsr_w[0] = lfsr_r[15] ^ lfsr_r[14] ^ lfsr_r[12] ^ lfsr_r[3];
+	end
+	endcase
+end
+
+// output transition
+always_comb begin
+	o_random_out_w = o_random_out_r;
+
+	case(state_r)
+	S_IDLE: begin
+		o_random_out_w = 4'd0;
+	end
+	S_PROC: begin
+		o_random_out_w = lfsr_r[3:0];
+	end
+	endcase
 end
 
 // ===== Sequential Circuits =====
 always_ff @(posedge i_clk or negedge i_rst_n) begin
 	// reset
 	if (!i_rst_n) begin
+		lfsr_r 		   <= 16'd1000_0000_0000_0000;
 		o_random_out_r <= 4'd0;
 		state_r        <= S_IDLE;
 	end
 	else begin
+		lfsr_r         <= lfsr_w;
 		o_random_out_r <= o_random_out_w;
 		state_r        <= state_w;
 	end
