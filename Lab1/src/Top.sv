e4dc9034568a0aa6d230a062c70450ddb08a1fde
shawntsai0312 <shawntsai0312@gmail.com>
2024-09-16 23:15:02 +0800

    final version code done

--- Lab1/src/Top.sv
+++ Lab1/src/Top.sv
@@ -19,10 +19,13 @@ parameter S_SHOW   = 3'd5;
 
 logic [15:0] SEED_r, SEED_w;
 // modify when uploading to FPGA, clock is 50MHz, clock period is 20ns
-parameter S_FAST_PERIOD   = 28'b1000_0000_0000_0000_0000_0000_0000;
-parameter S_MEDIUM_PERIOD = 28'b1000_0000_0000_0000_0000_0000_0000;
-parameter S_SLOW_PERIOD   = 28'b1000_0000_0000_0000_0000_0000_0000;
-parameter S_SHOW_PERIOD   = 28'b1000_0000_0000_0000_0000_0000_0000;
+parameter COUNTER_BITS = 26;
+parameter COUNTER_LOWER = 26'b0;
+parameter COUNTER_INC = 26'b1;
+
+parameter S_PROCESS_PERIOD = 26'b10_0000_0000_0000_0000_0000_0000;
+parameter S_SHOW_PERIOD   = 26'b01_0000_0000_0000_0000_0000_0000;
+
 
 // ===== Output Buffers =====
 logic [3:0] o_random_out_r, o_random_out_w;
@@ -30,7 +33,7 @@ logic [3:0] o_random_out_r, o_random_out_w;
 // ===== Registers & Wires =====
 logic [2:0] state_r, state_w;
 logic [15:0] lfsr_r, lfsr_w; // taps = 15, 14, 12, 3
-logic [27:0] counter_r, counter_w;
+logic [COUNTER_BITS-1:0] counter_r, counter_w;
 logic [3:0] lastResult_r, lastResult_w;
 
 // ===== Output Assignments =====
@@ -48,15 +51,15 @@ always_comb begin
 			end
 	S_FAST: begin
 		if(i_stop) 	state_w = S_DONE;
-		else 		state_w = (counter_r == S_FAST_PERIOD) ? S_MEDIUM : S_FAST;
+		else 		state_w = (counter_r == S_PROCESS_PERIOD) ? S_MEDIUM : S_FAST;
 	end
 	S_MEDIUM: begin
 		if(i_stop) 	state_w = S_DONE;
-		else 		state_w = (counter_r == S_MEDIUM_PERIOD) ? S_SLOW : S_MEDIUM;
+		else 		state_w = (counter_r == S_PROCESS_PERIOD) ? S_SLOW : S_MEDIUM;
 	end
 	S_SLOW: begin
 		if(i_stop) 	state_w = S_DONE;
-		else 		state_w = (counter_r == S_SLOW_PERIOD) ? S_DONE : S_SLOW;
+		else 		state_w = (counter_r == S_PROCESS_PERIOD) ? S_DONE : S_SLOW;
 	end
 	S_DONE: begin
 		if(i_start) state_w = S_FAST;
@@ -68,11 +71,12 @@ end
 
 // SEED transition
 always_comb begin
-	SEED_w = SEED_r;
-	case(state_r)
-	S_IDLE: SEED_w = (SEED_r == 16'b0)? 16'b1000_0000_0000_0000 : SEED_r + 1;
-	default: SEED_w = SEED_r;
-	endcase
+	// SEED_w = SEED_r;
+	// case(state_r)
+	// S_IDLE: SEED_w = (SEED_r == 16'b0)? 16'b1000_0000_0000_0000 : SEED_r + 1;
+	// default: SEED_w = SEED_r;
+	// endcase
+	SEED_w = (SEED_r == 16'b0)? 16'b1000_0000_0000_0000 : SEED_r + 1;
 end
 
 // lfsr transition
@@ -81,8 +85,8 @@ always_comb begin
 	lfsr_w = lfsr_r;
 
 	case(state_r)
-	S_IDLE: lfsr_w = SEED;
-	S_DONE: lfsr_w = SEED;
+	S_IDLE: lfsr_w = SEED_r;
+	S_DONE: lfsr_w = SEED_r;
 	default: begin
 		lfsr_w[15:1] = lfsr_r[14:0];
 		lfsr_w[0] = lfsr_r[15] ^ lfsr_r[14] ^ lfsr_r[12] ^ lfsr_r[3];
@@ -95,9 +99,9 @@ always_comb begin
 	counter_w = counter_r;
 
 	case(state_r)
-	S_IDLE:  counter_w = 28'd0;
-	S_DONE:  counter_w = 28'd0;
-	default: counter_w = (counter_r == 28'b1111_1111_1111_1111_1111_1111_1111) ? 28'd0 : counter_r + 28'd1;
+	S_IDLE:  counter_w = COUNTER_LOWER;
+	S_DONE:  counter_w = COUNTER_LOWER;
+	default: counter_w = (counter_r == S_PROCESS_PERIOD) ? COUNTER_LOWER : counter_r + COUNTER_INC;
 	endcase
 end
 
@@ -114,15 +118,15 @@ always_comb begin
 end
 
 // output transition
-logic [27:0] temp;
+logic [COUNTER_BITS-1:0] temp;
 always_comb begin
 	o_random_out_w = o_random_out_r;
-	temp = counter_r + 28'd1;
+	temp = counter_r + COUNTER_INC;
 	case(state_r)
 	S_IDLE: o_random_out_w = o_random_out_r +1;
-	S_FAST: 	if (temp[18]^counter_r[18])  o_random_out_w = lfsr_r[3:0];
-	S_MEDIUM: 	if (temp[20]^counter_r[20])  o_random_out_w = lfsr_r[3:0];
-	S_SLOW: 	if (temp[22]^counter_r[22])  o_random_out_w = lfsr_r[3:0];
+	S_FAST: 	if (temp[19]^counter_r[19])  o_random_out_w = lfsr_r[3:0];
+	S_MEDIUM: 	if (temp[21]^counter_r[21])  o_random_out_w = lfsr_r[3:0];
+	S_SLOW: 	if (temp[23]^counter_r[23])  o_random_out_w = lfsr_r[3:0];
 	S_DONE: o_random_out_w = i_show ? lastResult_r : o_random_out_r;
 	S_SHOW: o_random_out_w = (counter_r == S_SHOW_PERIOD) ? lastResult_r : o_random_out_r;
 	endcase
@@ -133,8 +137,8 @@ always_ff @(posedge i_clk or negedge i_rst_n) begin
 	// reset
 	if (!i_rst_n) begin
 		SEED_r         <= 16'b1000_0000_0000_0000;
-		lfsr_r 		   <= SEED;
-		counter_r	   <= 28'b0_0000_0000_0000_0000_0000_0000;;
+		lfsr_r 		   <= SEED_w;
+		counter_r	   <= COUNTER_LOWER;
 		o_random_out_r <= 4'd0;
 		state_r        <= S_IDLE;
 		lastResult_r   <= 4'd0;
