20b7cb84642f6457bbc7a11dbb401634f5248c1e
shawntsai0312 <shawntsai0312@gmail.com>
2024-10-22 22:42:52 +0800

    main abort

--- Lab3/src/AudPlayer.sv
+++ Lab3/src/AudPlayer.sv
@@ -1,7 +1,7 @@
 module AudPlayer (
     input i_rst_n,
     input i_bclk,
-    input i_daclrck, // send data to WM8731 when low
+    input i_daclrck, // 0 for left channel, 1 for right channel
     input i_en,
     input [15:0] i_dac_data,
     output o_aud_dacdat
